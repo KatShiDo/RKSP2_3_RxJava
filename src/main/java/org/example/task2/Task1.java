f149723ce0d4b1997cce57e3c963c29cc311ee44
Poberezhnyuk Nikita <katshido.2003@gmail.com>
2024-12-24 19:09:32 +0300

    final fix

--- src/main/java/org/example/task2/Task1.java
+++ src/main/java/org/example/task2/Task1.java
@@ -5,7 +5,7 @@ import io.reactivex.Observer;
 
 public class Task1 {
     public static void main(String[] args) {
-        Observable<Integer> integerStream = Observable.range(0, 1000);
-        integerStream.subscribe(x -> System.out.println("Number: " + x + "\tSquare: " + x * x));
+        Observable<Integer> integerObservable = Observable.range(0, 1000);
+        integerObservable.subscribe(x -> System.out.println("Number: " + x + "\tSquare: " + x * x));
     }
 }
